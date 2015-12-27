8ff262058a29033ae403f177165469671cf07109
Willy Rempel <willy.rempel@gmail.com>
2015-12-26 23:23:17 -0500

    updated UCI.java

--- chessEngine/chessEngine/UCI.java
+++ chessEngine/chessEngine/UCI.java
@@ -1,6 +1,11 @@
 package chessEngine;
 
-// UCI protocol implementation
+// UCI Protocol Implementation
+//
+// COSC 3P71 PROJECT
+// December, 2015
+// Willy Rempel		#9951674
+// Nick Seelert		#
 
 import java.util.Scanner;
 
@@ -8,6 +13,7 @@ public class UCI {
 
 	private static String		engineName = "Seelert&Rempel Engine v1";
 	private static String		author = "Willy Rempel & Nick Seelert";
+	private String				inputString;
 	
 	public UCI () {
 		
@@ -15,7 +21,7 @@ public class UCI {
  
         while (true)
         {
-            String inputString=input.nextLine();
+            inputString = input.nextLine();
             
             if (inputString.equals("uci"))
             {
@@ -41,42 +47,42 @@ public class UCI {
             {
             	commGo();
             }
-            else if (inputString.equals("quit"))
+            else if (inputString.equals("quit"))			// Close Engine.
             {
-                commQuit();
+                input.close();
+	            System.exit(0);
             }
-            else if (inputString.equals("print"))
-            {
-                commPrint();
-            }
-        }
+        }           
 	}
 
-	private void commPrint() {
-		// TODO Auto-generated method stub
-		
-	}
 
-	private void commQuit() {
-		System.exit(0);
-		
-	}
-
+	// Start calculating on the current position set up with the "position" command.
+	// There are a number of commands that can follow this command, all will be sent in the same string.
 	private void commGo() {
 		// TODO Auto-generated method stub
 		
 	}
 
+	// set up the position described in fenstring on the internal board and
+	// play the moves on the internal chess board.
+	// If the game was played from the start position the string "startpos" will be sent.
 	private void commPosition(String inputString) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	// This is sent to the engine when the next search (started with "position" and "go") will be from
+	// a different game. This can be a new game the engine should play or a new game it should analyse but
+	// also the next position from a testsuite with positions only.
 	private void commUCINewGame() {
 		// TODO Auto-generated method stub
 		
 	}
 
+	//	This is used to synchronize the engine with the GUI. When the GUI has sent a command or
+	//	multiple commands that can take some time to complete,
+	//	this command can be used to wait for the engine to be ready again or
+	//	to ping the engine to find out if it is still alive.
 	private void commIsReady() {
 		// TODO Auto-generated method stub
 		
@@ -87,8 +93,13 @@ public class UCI {
 		
 	}
 
+	// Tell the engine to use the UCI protocol
+	// Engine responds with id, author, options list, and 'uciok'
 	private void commUCI() {
-		// TODO Auto-generated method stub
+		System.out.println("id name " + engineName);
+		System.out.println("id author " + author);
+		System.out.println("options ");
+		System.out.println("uciok");
 		
 	}
 	
